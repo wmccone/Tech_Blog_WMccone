41eeef1b2281a6e913b6ce2d58802876d8dabc50
Gerry M <waltermccone88@gmail.com>
2021-04-06 21:00:02 -0500

    added a posts model

---
+++ models/Posts.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Posts extends Model {}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //stores subject of blog post
+    subject: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    //stores content of blog post
+    content_body: {
+      type: DataTypes.STRING,
+    },
+    //stores the date the blog post is created
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
+
+module.exports = Posts;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,6 @@
 const User = require('./User');
+const Posts = require('./Posts');
+
+
 
 module.exports = { User };
